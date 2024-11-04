626b0a337da76414f0eb8d0a8d6a51a734b4ab8a
saungnaga <saungnagaa@gmail.com>
2024-11-04 16:30:34 +0700

    fixing bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 import Header from './layout/header'
 import About from './layout/about'
 import Footer from './layout/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
